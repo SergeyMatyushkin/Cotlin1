f1c81002628a5591423ccea26ad51dc17882de9f
Sergey <gecktor87@gmail.com>
2021-06-30 21:38:23 +0300

    переделал на котлин

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,26 +1,17 @@
-package com.example.myapplication;
+package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-
-public class MainActivity extends AppCompatActivity {
-    private Button button;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, button.getText(), Toast.LENGTH_SHORT).show();
-
-            }
-        });
+class MainActivity : AppCompatActivity() {
+    private var button: Button? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button = findViewById(R.id.button)
+        button.setOnClickListener(View.OnClickListener { Toast.makeText(this@MainActivity, button.getText(), Toast.LENGTH_SHORT).show() })
     }
 }
